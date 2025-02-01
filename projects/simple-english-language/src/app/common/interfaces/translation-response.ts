fb02058bf06582e2bd2e0afc0b14fb7df5dab077
Eddie <macedoe@gmail.com>
2025-02-01 10:30:13 -0800

    refactor: update build scripts and simplify TranslationResponse interface

--- projects/simple-english-language/src/app/common/interfaces/translation-response.ts
+++ projects/simple-english-language/src/app/common/interfaces/translation-response.ts
@@ -1,55 +1,5 @@
 export interface TranslationResponse {
-    meta: {
-        id: string;
-        uuid: string;
-        lang: string;
-        src: string;
-        section: string;
-        stems: string[];
-        syns: string[];
-        ants: string[];
-        offensive: boolean;
-    };
-    hwi: {
-        hw: string;
-        prs: [
-            {
-                mw: string;
-                sound: {
-                    audio: string;
-                };
-            }
-        ];
-    };
-    fl: string;
-    def: [
-        {
-            sseq: [
-                [
-                    [
-                        string,
-                        {
-                            sn: string;
-                            dt: [
-                                string[],
-                                string[],
-                                string[],
-                                string[],
-                                [
-                                    string,
-                                    [
-                                        {
-                                            t: string;
-                                            tr: string;
-                                        }
-                                    ]
-                                ]
-                            ];
-                        }
-                    ]
-                ]
-            ];
-        }
-    ];
-    shortdef: string[];
+    id: string;
+    definitions: string[];
+    partOfSpeech: string;
 }
